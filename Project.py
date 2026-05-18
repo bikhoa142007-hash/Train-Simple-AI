f625110fc9ac56f184d91804a67b24ba19c8a212
bikhoa142007-hash <bikhoa142007@gmail.com>
2026-05-18 23:29:50 +0700

    commit

--- Project.py
+++ Project.py
@@ -4,11 +4,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import database as db
 
-
+feature_cols = [
+    'Gender', 
+    'Age', 
+    'Height', 
+    'Weight', 
+    'lumbar spine(L1-L4)', 
+    'BMD(Bone Mineral Density)', 
+    'T-score'
+]
 def main():
+    st.title("Dự đoán bệnh loãng xương")
     df = pd.read_csv("C:\\Users\\Khoa Bi\\Downloads\\du lieu.csv")
     df = df.dropna()
-    X = df.drop(columns=['Osteoporosis']).values
+    X = df[feature_cols]
     y = df['Osteoporosis'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model, scaler, poly, accuracy = db.train_until_reach_accuracy(X_train, y_train, target_accuracy=0.85)
@@ -16,7 +25,6 @@ def main():
     st.session_state.scaler = scaler
     st.session_state.poly = poly
     st.session_state.accuracy = accuracy
-    st.title("Dự đoán bệnh loãng xương")
     st.write("Nhập thông tin bệnh nhân để dự đoán nguy cơ loãng xương:")
     db.enter_input_page()
     if __name__ == "__main__":
